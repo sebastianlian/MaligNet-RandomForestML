51e437ce5708c5aee5d90fb089ef122485f39011
sebastianlian <scarmagnola@ggc.edu>
2025-02-23 07:21:40 -0500

    update

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,25 +1,12 @@
-from flask import Flask, jsonify
-from src.backend.models.linear_regression import results
-from src.backend.models.random_forest import rf_results
-# from src.backend.models.xgboost_model import xgb_results
+from flask import Flask
 
 app = Flask(__name__)
 
 @app.route("/", methods=["GET"])
-def home():
-    return jsonify({"message": "Flask API is running!"})
 
 @app.route("/regression", methods=["GET"])
-def regression_results():
-    return jsonify(results)
-
-@app.route("/random_forest", methods=["GET"])
-def random_forest_results():
-    return jsonify(rf_results)
-
-@app.route("/xgboost", methods=["GET"])
-def xgboost_results():
-    return jsonify(xgb_results)  # New endpoint for XGBoost
+def regression():
+    return jsonify(regression)
 
 if __name__ == "__main__":
     app.run(debug=True)
