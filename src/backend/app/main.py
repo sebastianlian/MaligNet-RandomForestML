a55d5f6d579b40e7c2f3b6871f54ba8699857a0c
sebastianlian <scarmagnola@ggc.edu>
2025-02-22 19:54:54 -0500

    create the route for the random forest results

--- src/backend/app/main.py
+++ src/backend/app/main.py
@@ -1,5 +1,6 @@
 from flask import Flask, jsonify
 from src.backend.models.linear_regression import results
+from src.backend.models.random_forest import rf_results
 
 app = Flask(__name__)
 
@@ -10,6 +11,10 @@ def home():
 @app.route("/regression", methods=["GET"])
 def regression_results():
     return jsonify(results)
+
+@app.route("/random_forest", methods=["GET"])
+def random_forest_results():
+    return jsonify(rf_results)
 
 if __name__ == "__main__":
     app.run(debug=True)
